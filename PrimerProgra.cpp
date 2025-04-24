e00adabc7ad179e86984e4165fdeb1a62434ef5c
AnderTurcios <00003925@uca.edu.sv>
2025-04-23 22:37:36 -0600

    9/abril, primer programa suma de dos numeros

---
+++ PrimerProgra.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+    int main(){
+    int Number1;
+    int Number2;
+
+    cout << "Hola mundo" <<endl;
+    cout << "Ingresa el primer numero" << endl;
+    cin >> Number1;
+
+    cout << "Ingresa el segundo numero" <<endl;
+    cin >> Number2;
+
+    int Resultado;
+
+if (Number1 == Number2) {
+    cout << "Los numeros son iguales" << endl;
+}   else {
+    
+    cout << "Los numeros NO son iguales" << endl;
+}
+
+return 0;
+    }
