59c62d62516d0904b618c2344efff43b3582e301
AnderTurcios <00003925@uca.edu.sv>
2025-06-02 20:05:53 -0600

    "Modified"

--- Arreglos.cpp
+++ Arreglos.cpp
@@ -2,25 +2,25 @@
 
 using namespace std;
 
-//Declarar varios valores en una variable
+// Declarar varios valores en una variable
 int main()
 {
-    
-    int numbers[5] = {1,2,3,4,5}; //Grupo de los valores de la variable
-    //En [] es la cantidad de espacios
-    
+
+    int numbers[5] = {1, 2, 3, 4, 5}; // Grupo de los valores de la variable
+    // En [] es la cantidad de espacios
+
     cout << "Contenido del arreglo" << endl;
-    
-    //Sirve para recorrer el arreglo y mostrar sus elementos
-    for (int i = 0; i <= 5; i++) //el i va en aumento bajo la condicion de i <= 5
+
+    // Sirve para recorrer el arreglo y mostrar sus elementos
+    for (int i = 0; i < 5; i++) // el i va en aumento bajo la condicion de i <= 5
     {
-        cout << "Elemento en la posicion " << i << ": " << numbers[i] << endl;
+        cout << "Elemento en la posicion " << i + 1 << ": " << numbers[i] << endl;
     }
-    
-//This is not working as it's supposed to
-    for (int is = 5; is >= 0; is--)
+    cout << "Siguiente" << endl;
+    // This is not working as it's supposed to
+    for (int j = 4; j >= 0; j--)
     {
-        cout << "Elemento en la posicion " << is << ": " << numbers[is] << endl;
-    } 
+        cout << "Elemento en la posicion " << j + 1 << ": " << numbers[j] << endl;
+    }
     return 0;
 }
